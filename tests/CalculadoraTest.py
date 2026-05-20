ed44f2bcfabb66eb0d4f225dec91b1ee29e8ca80
Igh0r <igorrafaelgauer@gmail.com>
2026-05-19 21:02:21 -0300

    atualização

--- tests/CalculadoraTest.py
+++ tests/CalculadoraTest.py
@@ -1,7 +1,22 @@
-from src.Calculadora import soma
+from src.Calculadora import soma, subtracao, multiplicacao, divisao
 
 def test_soma():
     assert soma(3, 3) == 9
-    assert soma(2, 9) == 6
+    assert soma(2, 9) == 11
     assert soma(0, 9) == 9
 
+def test_subtracao():
+    assert subtracao(3, 3) == 0
+    assert subtracao(2, 9) == -7
+    assert subtracao(0, 9) == -9
+
+def test_multiplicacao():
+    assert multiplicacao(3, 3) == 9
+    assert multiplicacao(2, 9) == 18
+    assert multiplicacao(0, 8) == 0
+
+def test_divisao():
+    assert divisao(9, 3) == 3
+    assert divisao(10, 2) == 5
+    assert divisao(0, 9) == 0
+    assert divisao(9, 0) == "Não é possível dividir por zero"
